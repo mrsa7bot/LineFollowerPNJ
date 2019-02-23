1ea4493a27e6f5ab51c85f0a9ceddfe5c2b99e0b
mrsa7bot <47033915+mrsa7bot@users.noreply.github.com>
2019-02-23 22:27:36 +0700

    Update SensorLine.ino

--- examples/SensorLine/SensorLine.ino
+++ examples/SensorLine/SensorLine.ino
@@ -1,15 +1,16 @@
 // include library
+
 #include <LineFollowerPNJ.h>;
 
 //inisialisasi instance line dengan pin 2 = selector 1, pin 3 = selector 2, pin 3 = selector 3, pin A0 = adc
 lineSensor line(7,8,9,A0); 
 
-void setup() 
+  void setup() 
 {
   //cara membaca sensor garis : instance.readLine; 
   Serial.begin(9600);
   Serial.println("Membaca Garis \n \n");
-  line.scanLine(6000);
+  line.scanLine(6000); //Scan Sensor Selama 6 detik
   delay(1000);
 }
 
